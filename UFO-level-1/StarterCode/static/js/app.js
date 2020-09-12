524c7811239f6e07e3d153cf148dce37027574f7
Katherine Schuman <katieschuman@katherinesair2.attlocal.net>
2020-09-12 11:58:56 -0400

    setting up table data

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,4 +1,30 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Put tbody into variable
+var tbody = d3.select("tbody");
+
+
+// Append new row for each ufo object (creating new trs)
+tableData.forEach(function (ufoSighting) {
+    // console.log(ufoSighting)
+    var row = tbody.append("tr");
+    // for each item in the 
+    Object.entries(ufoSighting).forEach(function[key, value] {
+        // Append cell for each value to the row 
+        var cell = row.append("td");
+        cell.text(value)
+    });
+});
+
+
+
+
+// // Event handler - attaching the function to the button
+
+// // 
+// button.on("click", function () {
+//     console.log(d3.event.target);
+// });
+
+// inputField.on("change", function () { }
