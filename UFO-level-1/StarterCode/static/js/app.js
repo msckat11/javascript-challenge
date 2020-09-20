9748e05ae50af0a76ad7cd8ad354df48abb2e6e4
Katherine Schuman <katieschuman@katherinesair2.attlocal.net>
2020-09-19 23:42:23 -0400

    added event handler code

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,5 +1,14 @@
-// from data.js
+// Assign variable to data from data.js
 var tableData = data;
+
+// // Define title case function
+// function titleCase(str) {
+//     // convert string to lowercase
+//     str = str.toLowerCase();
+//     str = str.split(" ");
+//     for (var i = 0; i < str.length; i++)
+// }
+
 
 // Put tbody into variable
 var tbody = d3.select("tbody");
@@ -23,7 +32,41 @@ tableData.forEach(function (ufoSighting) {
 
 
 
-// // Event handler - attaching the function to the button
+// Event handler - attaching the function to the button
+// Select the button
+var button = d3.select("#filter-btn");
+// Select the form
+var form = d3.select("#datetime");
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Create the function to run for both events
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Print the value to the console
+    console.log(inputValue);
+
+
+
+
+    // // Set the span tag in the h1 element to the text
+    // // that was entered in the form
+    // d3.select("h1>span").text(inputValue);
+}
+
+
+
 
 // // 
 // button.on("click", function () {
